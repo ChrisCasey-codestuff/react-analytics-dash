0489cc7b71c8df7b65bd8a7c75c0fb23affed8de
Christopher Casey <chris.casey1234@gmail.com>
2023-04-15 13:32:42 -0700

    completes  employees/orders/customers tables and functionality

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,7 +1,34 @@
-function SparkLine () {
-  return <div>
+import React from 'react'
+import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-react-charts';
 
-  </div>
+class SparkLine extends React.PureComponent
+{
+  render()
+  {
+  const {id, height, width, color, data, type, currentColor} = this.props
+   console.log(data)
+   return <SparklineComponent
+   id={id}
+   height={height}
+   width={width}
+   lineWidth={10}
+   valueType="Numeric"
+   type={type}
+   fill={color}
+   border={{color: currentColor, width: 2}}
+   dataSource={data}
+   xName="x"
+   yName="yVal"
+   tooltipSettings={{
+    visible: true,
+    format:'${x} : data ${y}',
+    trackLineSettings: { visible: true }
+   }}
+   >
+    <Inject services={[SparklineTooltip]} />
+  </SparklineComponent>
+ }
+
 }
 
 export default SparkLine;
--- src/components/Charts/Stacked.jsx
+++ src/components/Charts/Stacked.jsx
@@ -1,7 +1,32 @@
-function Stacked () {
-  return <div>
+import React from "react"
 
-  </div>
+import {ChartComponent, SeriesCollectionDirective,
+  SeriesDirective, Inject, Legend,
+  Category, StackingColumnSeries, Tooltip} from "@syncfusion/ej2-react-charts"
+
+import { stackedCustomSeries, stackedPrimaryXAxis, stackedPrimaryYAxis} from "../../data/dummy"
+
+class Stacked extends React.PureComponent
+{
+  render()
+  {
+    const {width, height} = this.props
+   return <ChartComponent
+   width={width}
+   height={height}
+   id="charts"
+   primaryXAxis={stackedPrimaryXAxis}
+   primaryYAxis={stackedPrimaryYAxis}
+   chartArea={{border:{ width:0 }}}
+   tooltip={{enable: true}}
+   legendSettings={{background:"white"}}>
+    <Inject services={[Legend, Category, StackingColumnSeries, Tooltip]}/>
+    <SeriesCollectionDirective>
+      {stackedCustomSeries.map((item, index) => <SeriesDirective key={index} {...item}/>)}
+    </SeriesCollectionDirective>
+   </ChartComponent>
+ }
+
 }
 
 export default Stacked;
