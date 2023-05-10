77fc46d87acd102164f707f8c654ed81a39adfb1
Christopher Casey <chris.casey1234@gmail.com>
2023-05-10 11:55:46 -0700

    cleans some dark mode css

--- src/components/ThemeSettings.jsx
+++ src/components/ThemeSettings.jsx
@@ -8,8 +8,7 @@ function ThemeSettings () {
   const { setColor, setMode, currentMode, currentColor, setThemeSettings } = useStateContext();
   return (
   <div className="bg-half-transparent dark:bg-dark-main-bg w-screen fixed nav-item top-0 right-0">
-    <div className="float-right h-screen dark:text-gray-200
-     bg-white dark:[#484B52] w-400">
+    <div className="float-right h-screen bg-white dark:[#484B52] w-400">
       <div className="flex justify-between items-center p-4 ml-4">
         <p className="font-semibold text-xl">Settings</p>
         <button
